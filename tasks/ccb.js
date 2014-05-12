72dc2342ade6efba532610b612cad2e91d56f058
Chris Ridde <criddle@opentable.com>
2014-05-12 11:40:04 +0100

    Fixed error logging

--- tasks/ccb.js
+++ tasks/ccb.js
@@ -15,27 +15,34 @@ module.exports = function(grunt){
 
         // todo: this should probably be passed in (as would be different for non-Opentable users)
         var createCcbContents = function(){
-
-            return {
-                fields: {
-                    project: {
-                        id: options.jira.project_id
-                    },
-                    summary: util.format('Deploying %s %s to production', options.project.name, options.build_number),
-                    issuetype: {
-                        id: options.jira.ccb_issue_type
-                    },
-                    customfield_11502: grunt.template.today("isoDateTime"),
-                    customfield_11505: 'Commit log:\n\n' + grunt.file.read(options.manifest)
-                }
-            };
-
+            try
+            {
+                return {
+                    fields: {
+                        project: {
+                            id: options.jira.project_id
+                        },
+                        summary: util.format('Deploying %s %s to production', options.project.name, options.build_number),
+                        issuetype: {
+                            id: options.jira.ccb_issue_type
+                        },
+                        customfield_11502: grunt.template.today("isoDateTime"),
+                        customfield_11505: 'Commit log:\n\n' + grunt.file.read(options.manifest)
+                    }
+                };
+            }
+            catch(err){
+                grunt.fail(err);
+            }
         };
 
         var createCbb = function(){
+            grunt.verbose.writeln("Creating CCB");
 
             var ccbContents = createCcbContents(),
                 deferred = q.defer();
+
+            grunt.verbose.writeln(JSON.stringify(ccbContents));
 
             request({
                 url: options.jira.api_url + "issue/",
@@ -51,14 +58,15 @@ module.exports = function(grunt){
                 },
                 json: ccbContents
             }, function(error, response, body){
-
-                if (response.statusCode >= 300 || error){
+                if (error) {
                     deferred.reject(error);
                 }
-                else{
+                else if (response.statusCode >= 300 ) {
+                    deferred.reject("Bad response: " + response);
+                }
+                else {
                     var ccbId = body.id;
                     grunt.log.writeln('CCB ID is: ' + ccbId);
-
                     deferred.resolve(ccbId);
                 }
             });
@@ -67,6 +75,7 @@ module.exports = function(grunt){
         };
 
         var updateCcbToInDevelopmentState = function(ccbId){
+            grunt.verbose.writeln("Updating CCB to 'In development' state");
 
             var deferred = q.defer();
 
@@ -88,20 +97,22 @@ module.exports = function(grunt){
                     }
                 }
             }, function(error, response, body){
-
-                if (error){
+                if (error) {
                     deferred.reject(error);
                 }
-                else{
+                else if (response.statusCode >= 300 ) {
+                    deferred.reject("Bad response: " + response);
+                }
+                else {
                     deferred.resolve(ccbId);
                 }
             });
 
             return deferred.promise;
-
         };
 
         var closeCcb = function(ccbId){
+            grunt.verbose.writeln("Updating CCB to 'Closed' state");
 
             var deferred = q.defer();
 
@@ -123,11 +134,13 @@ module.exports = function(grunt){
                     }
                 }
             }, function(error, response, body){
-
-                if (error){
+                if (error) {
                     deferred.reject(error);
                 }
-                else{
+                else if (response.statusCode >= 300 ) {
+                    deferred.reject("Bad response: " + response);
+                }
+                else {
                     deferred.resolve();
                 }
             });
@@ -137,12 +150,19 @@ module.exports = function(grunt){
         };
 
         createCbb()
-            .then(updateCcbToInDevelopmentState)
-            .then(closeCcb)
-            .catch(function(error){
-                grunt.fatal.fail(error);
+            .then(function(ccbId){
+                return updateCcbToInDevelopmentState(ccbId);
             })
-            .done(done);
+            .then(function(ccbId){
+                return closeCcb(ccbId);
+            })
+            .catch(function(error){
+                grunt.fatal(error);
+            })
+            .done(function(){
+                grunt.verbose.writeln("Done!");
+                done();
+            });
 
     });
 };
